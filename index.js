2c787da439ed2063d7ac6db792ffd5da0ed7ee2f
Maryann Ruguru Njuguna <maryannruguru20@gmail.com>
2022-11-25 23:30:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code in this file!
+const currentUser = 'Grass Hopper';
+
+typeof "const currentUser = 'Grass Hopper';";
+//=> "string"
+
+"const currentUser = 'Grass Hopper';".length;
+//=> 35
+currentUser;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+//const shortGreeting = 'Welcome, !'[0];
+
+//const welcomeMessage = 'Welcome to Flatbook, ';
+//const welcomeMessage = 'Welcome to Flatbook,  + ${currentUser}!';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+//const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
+//const excitedWelcomeMessage 
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
